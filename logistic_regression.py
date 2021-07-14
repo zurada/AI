d54f6e3c7a83652236fe8fc07a111c9999ef73d0
Adam Zurada <32738441+zurada@users.noreply.github.com>
2021-07-14 12:51:46 +0200

    Create logistic_regression.py

---
+++ logistic_regression.py
@@ -0,0 +1,68 @@
+# apt-get install python3-matplotlib
+# python3 logistic_regression.py
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+x1 = []
+x2 = []
+y = []
+for i in range(100):
+    x1.append(random.uniform(0, 7))
+    x2.append(random.uniform(0, 3))
+    y.append(0)
+
+for i in range(100):
+    x1.append(random.uniform(3, 10))
+    x2.append(random.uniform(4, 10))
+    y.append(1)
+
+errors = []
+b0 = 0   
+b1 = 0       
+b2 = 0       
+alpha = 0.01 
+e = 2.71828
+err = 0
+for i in range(2000000):
+    idx = i % 200
+    if idx == 0:
+        if err != 0:
+            errors.append(err)
+        err = 0
+    
+    gX = (b0 + b1 * (x1[idx]) + b2 * (x2[idx]))
+    hX = 1 / (1 + pow(e, -gX))
+    err += pow(hX - y[idx], 2)
+    
+    b0 -= alpha * (hX - y[idx])
+    b1 -= alpha * (hX - y[idx]) * x1[idx]
+    b2 -= alpha * (hX - y[idx]) * x2[idx]
+
+plt.figure(0)
+for i in range(len(y)):
+    print(i)
+    gX = (b0 + b1 * (x1[i]) + b2 * (x2[i]))
+    hX = 1 / (1 + pow(e, -gX))
+    print("hX", hX)
+    if hX > 0.5:
+        plt.scatter(x1[i], x2[i], s=100, c='green')
+    else:
+        plt.scatter(x1[i], x2[i], s=100, c='red')
+
+x_var = np.linspace(0,10,10000)
+y_var = (-b0 - b1*x_var) / b2 
+
+for i in range(len(y)):
+    gX = (b0 + b1 * (x_var[i]) + b2 * (y_var[i]))
+    hX = 1 / (1 + pow(e, -gX))
+    print("hX:", hX)
+    
+plt.plot(x_var, y_var, '-r', label='')
+
+plt.figure(1)
+x_var = np.linspace(0,1000000,len(errors))
+plt.plot(x_var, errors, '-r', label='')
+
+plt.grid()
+plt.show()
